80553433fb5b1ba96ac03f77754c02ea6cad6d06
Hansheng1219 <Hansheng1219@gmail.com>
2026-06-24 20:46:46 +0800

    add the function of use arrow

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from itertools import product
 
 
 class SudokuCell(tk.Entry):
@@ -20,6 +21,8 @@ class SudokuCell(tk.Entry):
         vcmd = (self.register(self._validate_input), "%P")
         self.config(validatecommand=vcmd)
 
+
+
     def _validate_input(self, P):
         if P == "":
             return True
@@ -39,30 +42,33 @@ class SudokuBoard(tk.Frame):
 
         # --- 四層迴圈邏輯 ---
         # b_row, b_col 代表 3x3 個「大區塊」的座標 (0~2)
-        for b_row in range(3):
-            for b_col in range(3):
-                # 1. 建立一個 3x3 的大區塊容器
-                # highlightbackground="black" 設定外框顏色
-                # highlightthickness=2 設定外框粗細 (這就是你要的粗線)
+        for b_row, b_col in product(range(3), range(3)):
+            # 1. 建立一個 3x3 的大區塊容器
+            # highlightbackground="black" 設定外框顏色
+            # highlightthickness=2 設定外框粗細 (這就是你要的粗線)
 
-                block_frame = tk.Frame(
-                    self, highlightbackground="black", highlightthickness=2, bd=0
-                )
-                block_frame.grid(row=b_row, column=b_col)
+            block_frame = tk.Frame(
+                self, highlightbackground="black", highlightthickness=2, bd=0
+            )
+            block_frame.grid(row=b_row, column=b_col)
 
-                # row, col 代表該區塊內「小格子」的相對座標 (0~2)
-                for r in range(3):
-                    for c in range(3):
-                        # 2. 計算這格在 9x9 盤面上的「絕對座標」
-                        actual_row = b_row * 3 + r
-                        actula_col = b_col * 3 + c
+            # row, col 代表該區塊內「小格子」的相對座標 (0~2)
+            for r, c in product(range(3), range(3)):
+                # 2. 計算這格在 9x9 盤面上的「絕對座標」
+                actual_row = b_row * 3 + r
+                actula_col = b_col * 3 + c
 
-                        # 3. 建立格子，注意！master 要設為 block_frame 而非 self
-                        cell = SudokuCell(block_frame, actual_row, actula_col)
-                        cell.grid(row=r, column=c)
+                # 3. 建立格子，注意！master 要設為 block_frame 而非 self
+                cell = SudokuCell(block_frame, actual_row, actula_col)
+                cell.grid(row=r, column=c)
 
-                        # 4. 依然存入 9x9 陣列中，確保 get/set_board_data 功能不受影響
-                        self.cells[actual_row][actula_col] = cell
+                # 4. 依然存入 9x9 陣列中，確保 get/set_board_data 功能不受影響
+                self.cells[actual_row][actula_col] = cell
+
+                cell.bind("<Up>", lambda event, r=actual_row, c=actula_col: self.move_focus(r - 1, c))
+                cell.bind("<Down>", lambda event, r=actual_row, c=actula_col: self.move_focus(r + 1, c))
+                cell.bind("<Left>", lambda event, r=actual_row, c=actula_col: self.move_focus(r, c - 1))
+                cell.bind("<Right>", lambda event, r=actual_row, c=actula_col: self.move_focus(r, c + 1))
 
             # self.cells.append(row_cell)
 
@@ -99,3 +105,10 @@ class SudokuBoard(tk.Frame):
                 val = data[r][c]
                 if val != 0:
                     self.cells[r][c].insert(0, str(val))
+
+    def move_focus(self, row, col):
+        new_r = max(0, min(8, row))
+        new_c = max(0, min(8, col))
+
+        target_cell = self.cells[new_r][new_c]
+        target_cell.focus_set()
